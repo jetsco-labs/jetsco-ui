4818f456846585248953a3001ad185520a48b22f
mgt <3550756510@qq.com>
2026-08-16 11:02:03 +0800

    handle edge cases

--- src/util.ts
+++ src/util.ts
@@ -1,3 +1,4 @@
 // Mon Aug 17 05:02:03 PM CST 2026 iteration 4
 // Mon Aug 17 05:02:03 PM CST 2026 iteration 8
 // Mon Aug 17 05:02:03 PM CST 2026 iteration 9
+// Mon Aug 17 05:02:03 PM CST 2026 iteration 10
